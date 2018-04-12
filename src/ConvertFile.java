f0ffa2a621a28200a8a8878f422c890157477c84
tranb7 <tranb7@wwu.edu>
2018-04-12 01:30:45 -0700

    Added more detail to convertFile comments

--- src/ConvertFile.java
+++ src/ConvertFile.java
@@ -2,6 +2,7 @@
 
 public class ConvertFile {
     //Takes a file to be read through and converted into a generalized version of the file.
+    //Will read line by line through file and call whichever function is suitable for that situation
     //Writes generalized version to a new file
     public static void textConverter (File assignment) {
 
